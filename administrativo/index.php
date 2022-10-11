874e7e5aaa0eea12a2453275a33d5744068cd4b1
Luan Felipe Rodrigues Sanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-10-11 13:49:37 -0300

    Como limpar URL com PHP

--- administrativo/index.php
+++ administrativo/index.php
@@ -1,44 +1,55 @@
 <?php
-//Receber a url
+
+// Receber a url
 $url = filter_input(INPUT_GET, "url", FILTER_DEFAULT);
+// var_dump($url);
 
-//Converter a string em array
-$url_path = explode("/", $url);
+// Incluir a biblioteca para limpar a URL
+include_once "./app/adms/lib/lib_clean_url.php";
+$url_clean = cleanUrl($url);
 
-//Verificar se existe a posicao 1 do array
+// Converte a string em array
+$url_path = explode("/", $url_clean);
+// var_dump($url_path);
+
+// Verificar se existe a posicao 1 no array
 if ((isset($url_path['0'])) and (!empty($url_path['0']))) {
     $path_page = $url_path['0'];
 } else {
-    $path_detail = "login";
+    $path_page = "login";
 }
-//Verificar se existe a posicao 2 do array
+// var_dump($path_page);
+
+// Verificar se existe a posicao 2 no array
 if ((isset($url_path['1'])) and (!empty($url_path['1']))) {
     $path_detail = $url_path['1'];
 } else {
     $path_detail = "";
 }
-?>
+// var_dump($path_detail);
 
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
+    <!-- Indicar que o site possui caracteres especiais -->
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adm</title>
+    <title>Adm - Celke</title>
 </head>
 
 <body>
     <?php
-    //Caso a pagina não existe, vai para login
+    // Verificar se tem o nome da pagina
     if (!empty($path_page)) {
+        // Verificar se existe o arquivo
         if (file_exists("app/adms/views/" . $path_page . ".php")) {
+            // Carregar a pagina
             include_once "app/adms/views/" . $path_page . ".php";
-        } else {
-            include_once "app/adms/views/login.php";
+        } else { // Se nao existir o arquivo, carrega a pagina de erro 
+            include_once "app/adms/views/404.php";
         }
-    } else {
+    } else { // Se nao tiver a pagina, carrega a pagina login
         include_once "app/adms/views/login.php";
     }
     ?>
